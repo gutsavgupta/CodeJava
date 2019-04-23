170c2bf2629b404c56246176d68546b6338b1bda
gutsavgupta <gutsavgupta@gmail.com>
2019-04-23 12:48:47 +0530

    Setting up java environment

---
+++ JavaExampleCode/HelloWorld-ch1/helloworld.java
@@ -0,0 +1,15 @@
+/* This is my first java project 
+ * This is a simple projram to print
+ * hello world in the cosole 
+*/
+
+class helloworld
+{
+    public static void main(String[] args) 
+    {
+        // Print Hello world //
+        String str = "Hello World !!";
+        System.out.println("hello world !!");
+        System.out.println(str);    
+    }
+}
